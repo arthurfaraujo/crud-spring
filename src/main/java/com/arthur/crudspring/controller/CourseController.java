981fae8b41dd37b6ab65a15c9684aef3c290eca4
arthur <arthurfelipe5567@gmail.com>
2024-04-28 00:33:47 -0300

    feat: add course removing

--- src/main/java/com/arthur/crudspring/controller/CourseController.java
+++ src/main/java/com/arthur/crudspring/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.arthur.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,10 @@ public class CourseController {
   @ResponseStatus(code = HttpStatus.CREATED)
   public Course createCourse(@RequestBody Course course) {
     return courseRepository.save(course);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public void deleteCourse(@PathVariable long id) {
+    courseRepository.deleteById(id);
   }
 }
