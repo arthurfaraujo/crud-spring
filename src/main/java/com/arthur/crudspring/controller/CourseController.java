f559086573e7dcf1a3688f73323515c58d684333
arthur <arthurfelipe5567@gmail.com>
2024-04-29 01:13:35 -0300

    feat: add update logic

--- src/main/java/com/arthur/crudspring/controller/CourseController.java
+++ src/main/java/com/arthur/crudspring/controller/CourseController.java
@@ -3,10 +3,12 @@ package com.arthur.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,12 +33,24 @@ public class CourseController {
 
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
-  public Course createCourse(@RequestBody Course course) {
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
+  }
+
+  @PutMapping(path = "/{id}")
+  @ResponseStatus(code = HttpStatus.OK)
+  public Course edit(@RequestBody Course course) {
     return courseRepository.save(course);
   }
 
   @DeleteMapping(path = "/{id}")
-  public void deleteCourse(@PathVariable long id) {
+  public void deleteById(@PathVariable long id) {
     courseRepository.deleteById(id);
+  }
+
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<Course> getById(@PathVariable long id) {
+    return courseRepository.findById(id).map(course -> ResponseEntity.ok().body(course))
+        .orElse(ResponseEntity.notFound().build());
   }
 }
