2ec2ed07abb3c02ee4ac5c30dd794a42f7750edc
arthur <arthurfelipe5567@gmail.com>
2024-05-17 16:56:59 -0300

    feat: add lessons entity

--- src/main/java/com/arthur/crudspring/CrudSpringApplication.java
+++ src/main/java/com/arthur/crudspring/CrudSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.arthur.crudspring.enums.Category;
 import com.arthur.crudspring.model.Course;
+import com.arthur.crudspring.model.Lesson;
 import com.arthur.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
@@ -26,6 +27,19 @@ public class CrudSpringApplication {
       Course c = new Course();
       c.setName("Spring Boot BootCamp");
       c.setCategory(Category.BACK_END);
+
+      Lesson l1 = new Lesson();
+      l1.setName("Spring Boot Introduction");
+      l1.setYoutubeUrl("6g8G9LhHx3s");
+      l1.setCourse(c);
+
+      Lesson l2 = new Lesson();
+      l2.setName("Spring Boot Configuration");
+      l2.setYoutubeUrl("6g8G9LhHx3s");
+      l2.setCourse(c);
+
+      c.getLessons().add(l1);
+      c.getLessons().add(l2);
 
       courseRepository.save(c);
     };
